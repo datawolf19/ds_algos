ca2820f85920dfb06789f566357caf41f758d02b
Wolfgang Morton <wolfgang.morton@gmail.com>
2020-05-09 19:04:51 -0700

    doubly linked lists

---
+++ Lists & Pointers/doubly_linked_lists.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next 
+        self.prev = prev 
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+        
